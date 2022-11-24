db7bd5a3b6133b8be8ce48936173e31a7ed930e6
behradbhrmi <behrad.bhrmi@gmail.com>
2022-11-24 22:02:23 +0330

    optimize

--- maze.py
+++ maze.py
@@ -1,10 +1,18 @@
 
-_map =  [
-    [0, 1, 1],
-    [0, 1, 0],
-    [0, 0, 0],
-  ]
+# _map =  [
+#     [0, 0, 1, 1],
+#     [0, 1, 0, ],
+#     [0, 0, 0],
+#   ]
 
+_map = [
+    [0, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0],
+    [1, 0, 1, 0, 0],
+    [1, 1, 0, 1, 0],
+
+
+]
 path_stack = []
 
 col = len(_map) -1
@@ -12,11 +20,24 @@ row = len(_map[0]) -1
 
 found = False
 
-def maze(x, y, finish_x, finish_y):
+def printer(input):
+    for row in input:
+        for col in row:
+            print(col, end=' ')
+        print()
+
+def maze(y, x, finish_y, finish_x):
+    global found
+    if found == True:
+        return 
 
     if x > row or y > col : 
         return
+
     if x < 0 or y < 0 :
+        return
+    
+    if [y, x] in path_stack:
         return
     
     val = _map[y][x]
@@ -24,26 +45,31 @@ def maze(x, y, finish_x, finish_y):
     if val == 1 :
         return 
 
-    global found
-    global path_stack
+    path_stack.append([y, x])
 
-    _map[y][x] = 
+    if x == finish_x:
+        print('x is ok')
+    if y == finish_y :
+        print('y is ok')
     if x == finish_x and y == finish_y :
         found = True
-        return path_stack
-    path_stack.append([x, y])
+        print('done')
+        return 
     
-    maze(x  , y+1, finish_x, finish_y)
-    # maze(x+1, y+1, finish_x, finish_y)
-    maze(x+1, y  , finish_x, finish_y)
-    # maze(x+1, y-1, finish_x, finish_y)
-    maze(x  , y-1, finish_x, finish_y)
-    # maze(x-1, y-1, finish_x, finish_y)
-    maze(x-1, y  , finish_x, finish_y)
-    # maze(x-1, y+1, finish_x, finish_y)
+    maze(y+1, x  , finish_y, finish_x)
+    maze(y+1, x+1, finish_y,  finish_x)
+    maze(y ,x+1, finish_y, finish_x)
+    maze(y-1, x+1, finish_y, finish_x)
+    maze(y-1, x, finish_y, finish_x)
+    maze(y-1, x-1, finish_y, finish_x)
+    maze(y, x-1, finish_y, finish_x)
+    maze(y+1, x-1, finish_y, finish_x)
 
-    # if found == False:
-    #     path_stack = []
+def main():
+    maze(0, 0, 4, 5)
+    printer(_map)
+    print()
+    print(path_stack)
+    print(found)
 
-print(maze(0, 0, 2, 1))
-print(_map)
+main()
