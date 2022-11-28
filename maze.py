7d755f5ff9878d487334c022a4d8a433b9b7801d
behradbhrmi <behrad.bhrmi@gmail.com>
2022-11-28 15:49:06 +0330

    fix and add correct path printer

--- maze.py
+++ maze.py
@@ -1,5 +1,5 @@
+from copy import deepcopy as dc
 from os import system
-
 
 
 _map = [
@@ -14,11 +14,6 @@ row = len(_map[0]) -1
 found = False
 
 
-def printer(input):
-    for row in input:
-        print(*row)
-
-
 def maze(y, x, finish_y, finish_x):
     global found
     if finish_y > col or finish_x > row :
@@ -31,13 +26,11 @@ def maze(y, x, finish_y, finish_x):
     
     val = _map[y][x]
     if val == 1 : return 
-
     path_stack.append([y, x])
 
     if  y == finish_y and x == finish_x:
         found = True
         return 
-    
     maze(y+1, x  , finish_y, finish_x)
     maze(y+1, x+1, finish_y,  finish_x)
     maze(y ,x+1, finish_y, finish_x)
@@ -53,13 +46,30 @@ def maze(y, x, finish_y, finish_x):
             path_stack.append(l)
 
 
+def printer(input_list):
+    for row in input_list:
+        print(*row)
+
+
+def correct_path_printer():
+    new_list = dc(_map)
+    for j, item1 in enumerate(new_list):
+        for i, item2 in enumerate(item1) :
+            if [j, i] in path_stack:
+                new_list[j][i] = 1
+                continue
+            new_list[j][i] = 0
+    printer(new_list)
+    return new_list
+
+
 def main():
     system('cls')
     print('---Map---')
     printer(_map)
     print('------')
 
-    print('\n>> Enter the Starting Point Coordinates :')
+    print('>> Enter the Starting Point Coordinates :')
     start_y =  int(input('[y]: '))
     start_x =  int(input('[x]: '))
 
@@ -69,8 +79,11 @@ def main():
 
     maze(start_y, start_x, finish_y, finish_x)
 
-    print('------')
-    if found == True: print('\nCorrect Path is :\n', *path_stack)
+    if found == True: 
+        print('\n--Correct Path--')
+        correct_path_printer()
+        print("--List--")
+        print(*path_stack,"\n")
     else: print('> Could Not Reach Finishing Point :(')
 
 
